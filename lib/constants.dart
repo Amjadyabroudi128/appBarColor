daf73b6d6a4a30499a2ba195733a9da6ee4c0ff4
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-09-05 12:18:21 +0100

    icon

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,7 @@ import 'package:flutter/material.dart';
 
  //Appbar
 Color appBar = Colors.blue;
+Color iconclr = Colors.black;
+
+// icon
+Icon popmenu = Icon(Icons.arrow_downward, color: iconclr);
